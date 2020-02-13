b0d81959fb1475e4eb3599512154d05c9c094b88
Predrag <pedja.kostic@hotmil.com>
2020-02-13 11:00:08 +0100

    Real Estate Start Project

---
+++ resources/views/queez/partials/bottomnavbar.blade.php
@@ -0,0 +1,176 @@
+
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-sm-8 col-12">
+                            <div class="header-top-left">
+                                <ul>
+                                    <li><i class="fa fa-phone" aria-hidden="true"></i> <a href="tel:4444356348">+444 435 6348</a></li>
+                                    <li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:info@sarchholm.com">info@sarchholm.com</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 col-12">
+                            <div class="menu-btn">
+                                <ul class="user-btn">
+                                    <li><a href="#"><i class="fa fa-arrows-h" aria-hidden="true"></i></a>
+                                        <span>3</span>
+                                    </li>
+                                    <li><a href="#"><i class="lnr lnr-heart"></i></a>
+                                        <span>0</span>
+                                    </li>
+                                    <li><a href="#" data-toggle="modal" data-target="#user-login-popup"><i class="lnr lnr-user"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"href="#"><img src="{{ url('realestate/images/logo-blue.png') }}" alt="Grazia logo" class="logo"></a></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Home
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Home Hero</a>
+          <a class="dropdown-item" href="#">Home Tab</a>
+          <a class="dropdown-item" href="#">Home Video</a>
+          <a class="dropdown-item" href="#">Home Paralax</a>
+          <a class="dropdown-item" href="#">Home Slider</a>
+          <a class="dropdown-item" href="#">Home Map</a>
+          <a class="dropdown-item" href="#">Home Single Property</a>
+        </div>
+          </li>
+          <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Listings
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <ul class="dropdown">
+            <li class="has-children">
+                <a href="#">List Layout</a>
+                <ul class="dropdown sub-menu">
+                    <li><a href="list-fullwidth.html">Full Width</a></li>
+                    <li><a href="list-fullwidth-map.html">Fullwidth with google map</a></li>
+                    <li><a href="list-fullwidth-leaflet-map.html">Fullwidth with Leaflet map</a></li>
+                    <li><a href="list-left-sidebar.html">Left Sidebar</a></li>
+                    <li><a href="list-right-sidebar.html">Right Sidebar</a></li>
+                </ul>
+            </li>
+            <li class="has-children">
+                <a href="#">Grid Layout</a>
+                <ul class="dropdown sub-menu">
+                    <li><a href="grid-fullwidth.html">Full Width</a></li>
+                    <li><a href="grid-fullwidth-map.html">Fullwidth with google map</a></li>
+                    <li><a href="grid-fullwidth-leaflet-map.html">Fullwidth with leaflet map</a></li>
+                    <li><a href="grid-two-column.html">Grid two column</a></li>
+                    <li><a href="grid-left-sidebar.html">Left Sidebar</a></li>
+                    <li><a href="grid-right-sidebar.html">Right Sidebar</a></li>
+                </ul>
+            </li>
+            <li class="has-children">
+                <a href="#">Map Layout - google</a>
+                <ul class="dropdown sub-menu">
+                    <li><a href="list-left-sidemap.html">List Left sidemap</a></li>
+                    <li><a href="list-right-sidemap.html">List Right sidemap</a></li>
+                    <li><a href="grid-left-sidemap.html">Grid Left sidemap</a></li>
+                    <li><a href="grid-right-sidemap.html">Grid Right sidemap</a></li>
+                    <li><a href="grid-search-half-map.html">advanced search</a></li>
+                </ul>
+            </li>
+            <li class="has-children">
+                <a href="#">Tab Layout</a>
+                <ul class="dropdown sub-menu">
+                    <li><a href="tab-fullwidth.html">Tab fullwidth</a></li>
+                    <li><a href="tab-left-sidebar.html">Tab left sidebar</a></li>
+                    <li><a href="tab-right-sidebar.html">Tab right sidebar</a></li>
+                </ul>
+            </li>
+            <li class="has-children">
+                <a href="#">Map Layout - Leaflet</a>
+                <ul class="dropdown sub-menu">
+                    <li><a href="list-left-leaflet-sidemap.html">List Left sidemap</a></li>
+                    <li><a href="list-right-leaflet-sidemap.html">List Right sidemap</a></li>
+                    <li><a href="grid-left-leaflet-sidemap.html">Grid Left sidemap</a></li>
+                    <li><a href="grid-right-leaflet-sidemap.html">Grid Right sidemap</a></li>
+                    <li><a href="grid-search-half-leafletmap.html">advanced search</a></li>
+                </ul>
+            </li>
+        </ul>
+                                            
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Property
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Single Property Hello Header</a>
+          <a class="dropdown-item" href="#">Single Property Video Header</a>
+          <a class="dropdown-item" href="#">Single Property Prallax Header</a>
+          <a class="dropdown-item" href="#">Single Property SLider Header v1</a>
+          <a class="dropdown-item" href="#">Single Property SLider Header v2</a>
+
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Pages
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+   <a class="dropdown-item" href="#">Agent</a>
+            <ul class="dropdown sub-menu">
+                 <li><a href="agent-list.html">Agent List</a>
+                 </li>
+                 <li><a href="agent-grid.html">Agent Grid</a>
+                 </li>
+                 <li><a href="single-agent.html">Agent Profile</a>
+                 </li>
+            </ul>
+          <a class="dropdown-item" href="#">Agency</a>
+            <ul class="dropdown sub-menu">
+                 <li><a href="agency-list.html">Agency List</a>
+                 </li>
+                 <li><a href="agency-grid.html">Agency Grid</a>
+                 </li>
+                 <li><a href="single-agency.html">Agency Profile</a>
+                 </li>
+            </ul>
+          <a class="dropdown-item" href="#">Dashboard</a>
+          <a class="dropdown-item" href="#">About Us</a>
+          <a class="dropdown-item" href="#">Contact Us</a>
+          <a class="dropdown-item" href="#">Membershio Packages</a>
+          <a class="dropdown-item" href="#">Compare</a>
+          <a class="dropdown-item" href="#">404 eror</a>
+          <a class="dropdown-item" href="#">Add Listings</a>
+          <a class="dropdown-item" href="#">FAQ</a>
+        </div>
+
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          News
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">News Left Sidebar</a>
+          <a class="dropdown-item" href="#">News Right Sidebar</a>
+          <a class="dropdown-item" href="#">News Without Sidebar</a>
+          <a class="dropdown-item" href="#">Single News One</a>
+          <a class="dropdown-item" href="#">Single News Two</a>
+          <a class="dropdown-item" href="#">Single News Three</a>
+        </div>
+      </li>
+    </ul>
+      <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">
+       <i class="fa fa-home" aria-hidden="true"></i> 
+       Add Lissting</button>
+    
+  </div>
+</nav>
